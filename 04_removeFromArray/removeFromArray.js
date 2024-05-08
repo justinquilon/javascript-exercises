e6f82bfb394808d7480354f577f2ab2c15af1bed
Justin <justinquilon@gmail.com>
2024-05-08 14:19:06 +0800

    updated js ex-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let removeItemsFromIndex = [];
+  for (let i = 1; i < arguments.length; i++) {
+    for (let j = 0; j < arguments[0].length; j++) {
+      if (arguments[0][j] === arguments[i]) {
+        removeItemsFromIndex.push(j);
+      }
+    }
+  }
 
+  removeItemsFromIndex.sort();
+
+  for (let k = removeItemsFromIndex.length - 1; k >= 0; k--) {
+    arguments[0].splice(removeItemsFromIndex[k], 1);
+  }
+
+  return arguments[0];
 };
+
+// console.log(removeFromArray([1, 2, 3], 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
