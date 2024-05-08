0c99a0221f7baf06e194fdd10791ba5663d9a56f
Justin <justinquilon@gmail.com>
2024-05-08 15:03:20 +0800

    updated js ex-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  let sum = 0;
+  let smallNum = 0;
+  let bigNum = 0;
 
+  if (
+    firstNum < 0 ||
+    secondNum < 0 ||
+    isNaN(firstNum) ||
+    isNaN(secondNum) ||
+    typeof firstNum == "string" ||
+    typeof secondNum == "string" ||
+    firstNum === Array ||
+    secondNum === Array
+  ) {
+    return "ERROR";
+  }
+
+  if (firstNum > secondNum) {
+    bigNum = firstNum;
+    smallNum = secondNum;
+  } else {
+    bigNum = secondNum;
+    smallNum = firstNum;
+  }
+
+  for (i = smallNum; i <= bigNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
