2eff8b5b065127513e5b570ea748c726c5f82f97
Justin <justinquilon@gmail.com>
2024-05-29 15:25:30 +0800

    added javascript ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	const arrayTotal = array.reduce((total, number) => {
+		return total + number;
+	}, 0);
+	return arrayTotal;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	const arrayTotal = array.reduce((total, number) => {
+		return total * number;
+	}, 1);
+	return arrayTotal;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+f = [];
+
+const factorial = function (num) {
+	if (num == 0 || num == 1) return 1;
+	if (f[num] > 0) return f[num];
+	return (f[num] = factorial(num - 1) * num);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
