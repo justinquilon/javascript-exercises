8b2f7bcaa1d0d095b6ca92bdfd807879516bba9f
Justin <justinquilon@gmail.com>
2024-05-30 18:08:34 +0800

    feat: updated javascript ex 11

---
+++ 11_getTheTitles/test.js
@@ -0,0 +1,14 @@
+const getTheTitles = require("./getTheTitles");
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+getTheTitles(books);
