a3b7e8a32a8e0878a9d6cafb7c21d09eac9c488f
Justin <justinquilon@gmail.com>
2024-05-31 19:32:03 +0800

    feat: updated javascript ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldest = people.sort(function (a, b) {
+    if (a.yearOfDeath === undefined) {
+      const lastGuy = new Date().getFullYear() - a.yearOfBirth;
+      const nextGuy = b.yearOfDeath - b.yearOfBirth;
+      return lastGuy > nextGuy ? 1 : -1;
+    } else if (b.yearOfDeath === undefined) {
+      const lastGuy = a.yearOfDeath - a.yearOfBirth;
+      const nextGuy = new Date().getFullYear() - b.yearOfBirth;
+      return lastGuy > nextGuy ? 1 : -1;
+    } else {
+      const lastGuy = a.yearOfDeath - a.yearOfBirth;
+      const nextGuy = b.yearOfDeath - b.yearOfBirth;
+      return lastGuy > nextGuy ? 1 : -1;
+    }
+  });
+  return Object.assign(...oldest.slice(-1));
 };
 
 // Do not edit below this line
---
+++ 12_findTheOldest/test.js
@@ -0,0 +1,53 @@
+const findTheOldest = require("./findTheOldest");
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
+
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 2018,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+console.log(findTheOldest(people).name);
